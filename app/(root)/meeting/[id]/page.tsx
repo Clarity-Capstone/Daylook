3c107313bbcd007ffe137ab6d51422daa007755c
Rashell <rashellaldas@Rashells-MacBook-Pro.local>
2025-01-22 18:38:37 -0500

    setup and files done

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default async function Meeting({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const id = (await params).id
+  return <div>Meeting Room: #{id}</div>
+}
+
+//syncronous code and the app expects params to be avaliable imediatly
+// if we use a prmoise it makes it so coide is asynronous, so if param is not avalibe the app will run other code 
