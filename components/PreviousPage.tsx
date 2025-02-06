6a5e562137011eac23d0357922e5da53468f8e64
Fiona Baci <fionabaci04@gmail.com>
2025-02-06 11:07:32 -0500

    previous Meeting

--- components/PreviousPage.tsx
+++ components/PreviousPage.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import CallList from "./CallList";
 
 const Prev = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Previous Meetings</h1>
+
+    <CallList type='ended'/>
     </section>
   );
 };
